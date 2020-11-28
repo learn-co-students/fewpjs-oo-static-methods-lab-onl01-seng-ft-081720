64efdf56ac6a5b1c177048091aa9644af8ce493e
ken025 <kennethmanuel250@gmail.com>
2020-11-28 01:54:13 -0500

    Done.

--- index.js
+++ index.js
@@ -9,23 +9,23 @@ class Formatter {
   }
 
   static titleize(string) {
-    const titleize = require('titleize');
-    return titleize('string');
-    // let result = [];
-    // let except = [ 'the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from' ]
-    // let arrayOfWords =string.split( " " )
-    // for ( let n = 0; n < arrayOfWords.length; n++ ) {
-    //   if ( n == 0 ) {
-    //     result.push( this.capitalize( arrayOfWords[ n ] ) )
-    //   } else {
-    //     if ( except.includes( arrayOfWords[ n ] ) ) {
-    //       result.push( arrayOfWords[ n ] )
-    //     } else {
-    //       result.push( this.capitalize( arrayOfWords[ n ] ) )
-    //     }
-    //   }
+    // const titleize = require('titleize');
+    // return titleize(string);
+    let result = [];
+    let except = [ 'the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from' ]
+    let arrayOfWords =string.split( " " )
+    for ( let n = 0; n < arrayOfWords.length; n++ ) {
+      if ( n == 0 ) {
+        result.push( this.capitalize( arrayOfWords[ n ] ) )
+      } else {
+        if ( except.includes( arrayOfWords[ n ] ) ) {
+          result.push( arrayOfWords[ n ] )
+        } else {
+          result.push( this.capitalize( arrayOfWords[ n ] ) )
+        }
+      }
 
-    // }
-    // return result.join( " " );
+    }
+    return result.join( " " );
   }
 }
