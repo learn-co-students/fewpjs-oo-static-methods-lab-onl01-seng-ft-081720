94e5265423149ba64630a79c9c5d198a1c472fb7
Tabatha Slatton <valbatha@gmail.com>
2020-11-27 23:54:34 -0700

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-'\s]+/g, '');
+  }
+  static titleize(string) {
+    let spl = string.split(" ")
+    let except = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let result = []
+    for ( let n = 0; n < spl.length; n++ ) {
+      if ( n == 0 ) {
+        result.push( this.capitalize( spl[ n ] ) )
+      } else {
+        if ( except.includes( spl[ n ] ) ) {
+          result.push( spl[ n ] )
+        } else {
+          result.push( this.capitalize( spl[ n ] ) )
+        }
+      }
+
+    }
+    return result.join( " " );
+  }
 }
