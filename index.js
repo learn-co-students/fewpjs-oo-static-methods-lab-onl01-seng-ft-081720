01b611dd8d4d32eb62b92e2f7971939e06a1bdc0
ellenupark <ellenupark@gmail.com>
2020-12-01 21:48:59 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 class Formatter {
-  //add static methods here
+  static capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1)
+  }
+
+  static sanitize(str) {
+    return str.replace(/[^-'\sA-Za-z0-9]+/g, '');
+  }
+
+  static titleize(str) {
+    const invalid = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    return str.split(" ").map(function(word, i) {
+      if (i === 0) {
+        return word.charAt(0).toUpperCase() + word.slice(1);
+      } else if (invalid.includes(word)) {
+        return word;
+      } else {
+        return word.charAt(0).toUpperCase() + word.slice(1);
+      }
+    }).join(" ")
+  }
 }
